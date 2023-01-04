503b7aae58a1b7ca4f7468c8460af7f216591247
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-01-04 14:35:08 +0900

    Add new elements

--- src/main/kotlin/hiiragi283/ragi_materials/materials/EnumMaterials.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/materials/EnumMaterials.kt
@@ -7,11 +7,12 @@ import java.awt.Color
 enum class EnumMaterials(val index: Int, val registryName: String, val type: MaterialTypes, val color: Color) {
 
     //0: FOR DEBUG
-    DEBUG(0, "hiiragi_tsubasa", MaterialTypes.METAL, RagiColor.RAGI_RED),
+    DEBUG(0, "hiiragi_tsubasa", MaterialTypes.WILDCARD, RagiColor.RAGI_RED),
 
     //1~100: Periodic Table
     HYDROGEN(1, "hydrogen", MaterialTypes.GAS, Color.BLUE),
     HELIUM(2, "helium", MaterialTypes.GAS, Color.YELLOW),
+
     LITHIUM(3, "lithium", MaterialTypes.METAL, RagiColor.CLEAR),
     BERYLLIUM(4, "beryllium", MaterialTypes.METAL, Color.GREEN),
     BORON(5, "boron", MaterialTypes.DUST, Color.GRAY),
@@ -20,6 +21,7 @@ enum class EnumMaterials(val index: Int, val registryName: String, val type: Mat
     OXYGEN(8, "oxygen", MaterialTypes.GAS, Color.CYAN),
     FLUORINE(9, "fluorine", MaterialTypes.GAS, Color.GREEN),
     NEON(10, "neon", MaterialTypes.GAS, Color.MAGENTA),
+
     SODIUM(11, "sodium", MaterialTypes.METAL, RagiColor.CLEAR),
     MAGNESIUM(12, "magnesium", MaterialTypes.METAL, RagiColor.CLEAR),
     ALUMINIUM(13, "aluminium", MaterialTypes.METAL, RagiColor.CLEAR),
@@ -29,6 +31,18 @@ enum class EnumMaterials(val index: Int, val registryName: String, val type: Mat
     CHLORINE(17, "chlorine", MaterialTypes.GAS, Color.YELLOW),
     ARGON(18, "argon", MaterialTypes.GAS, Color.MAGENTA),
 
+    POTASSIUM(19, "potassium", MaterialTypes.METAL, RagiColor.CLEAR),
+    CALCIUM(20, "calcium", MaterialTypes.METAL, RagiColor.CLEAR),
+    TITANIUM(22, "titanium", MaterialTypes.METAL, RagiColor.CLEAR),
+    CHROMIUM(24, "chromium", MaterialTypes.METAL, RagiColor.CLEAR),
+    MANGANESE(25, "manganese", MaterialTypes.METAL, RagiColor.CLEAR),
+    IRON(26, "iron", MaterialTypes.METAL, RagiColor.CLEAR),
+    COBALT(27, "cobalt", MaterialTypes.METAL, RagiColor.CLEAR),
+    NICKEL(28, "nickel", MaterialTypes.METAL, RagiColor.CLEAR),
+    COPPER(29, "copper", MaterialTypes.METAL, Color.ORANGE),
+    ZINC(30, "zinc", MaterialTypes.METAL, RagiColor.CLEAR),
+    GALLIUM(31, "gallium", MaterialTypes.METAL, RagiColor.CLEAR),
+    ARSENIC(33, "arsenic", MaterialTypes.DUST, Color.GRAY),
 
     //32768: WILDCARD
     WILDCARD(32768, "wildcard", MaterialTypes.WILDCARD, RagiColor.CLEAR)
