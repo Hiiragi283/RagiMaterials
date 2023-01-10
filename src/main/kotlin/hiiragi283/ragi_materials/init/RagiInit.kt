a890e546b4695ac1ae2b05e3a6c74d2a18548543
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-01-10 19:41:37 +0900

    Override properties of vanilla blocks

--- src/main/kotlin/hiiragi283/ragi_materials/init/RagiInit.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/init/RagiInit.kt
@@ -3,6 +3,7 @@ package hiiragi283.ragi_materials.init
 import hiiragi283.ragi_materials.event.ItemTooltip
 import hiiragi283.ragi_materials.event.RightClickBlock
 import hiiragi283.ragi_materials.util.RagiUtils
+import net.minecraft.init.Blocks
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.registry.ForgeRegistries
 
@@ -10,7 +11,7 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries
 object RagiInit {
 
     fun loadPreInit() {
-        //Block, Event, Itemの登録
+        //Block, Event, Fluid, Itemの登録
         RagiInitBlock.registerBlocks()
         registerEvents()
         RagiInitFluid.registerFluids()
@@ -25,17 +26,57 @@ object RagiInit {
     }
 
     fun loadPostInit() {
-        //Itemの最大スタック数を上書きする
-        for (name in ForgeRegistries.ITEMS.keys) {
-            val item = RagiUtils.getItem(name.toString())
-            //itemの耐久値が0の場合、最大スタック数を64に上書きする
-            if (item.maxDamage == 0) item.setMaxStackSize(64)
-        }
+        overrideProperty()
+        overrideStack()
     }
 
     //Eventを登録するメソッド
     private fun registerEvents() {
         MinecraftForge.EVENT_BUS.register(ItemTooltip())
         MinecraftForge.EVENT_BUS.register(RightClickBlock())
+    }
+
+    //Vanillaのブロックのプロパティを上書きするメソッド
+    private fun overrideProperty() {
+        //光源レベルの上書き
+        //イワライナー氏から着想を得ました
+        Blocks.BROWN_MUSHROOM.setLightLevel(0.0F)
+        Blocks.LIT_REDSTONE_ORE.setLightLevel(0.0F)
+        Blocks.POWERED_COMPARATOR.setLightLevel(0.0F)
+        Blocks.POWERED_REPEATER.setLightLevel(0.0F)
+        Blocks.REDSTONE_TORCH.setLightLevel(0.0F)
+        Blocks.REDSTONE_WIRE.setLightLevel(0.0F)
+        //光の透過率を上書きする
+        Blocks.FLOWING_LAVA.setLightOpacity(0)
+        Blocks.FLOWING_WATER.setLightOpacity(0)
+        Blocks.ICE.setLightOpacity(0)
+        Blocks.LAVA.setLightOpacity(0)
+        Blocks.WATER.setLightOpacity(0)
+        //採掘レベルの新規追加
+        Blocks.CACTUS.setHarvestLevel("axe", 0)
+        Blocks.CARPET.setHarvestLevel("axe", 0)
+        Blocks.GLASS.setHarvestLevel("pickaxe", 0)
+        Blocks.GLASS_PANE.setHarvestLevel("pickaxe", 0)
+        Blocks.HAY_BLOCK.setHarvestLevel("axe", 0)
+        Blocks.LEAVES2.setHarvestLevel("axe", 0)
+        Blocks.LEAVES.setHarvestLevel("axe", 0)
+        Blocks.LEVER.setHarvestLevel("pickaxe", 0)
+        Blocks.NETHER_WART_BLOCK.setHarvestLevel("axe", 0)
+        Blocks.PISTON.setHarvestLevel("pickaxe", 0)
+        Blocks.SEA_LANTERN.setHarvestLevel("pickaxe", 0)
+        Blocks.SKULL.setHarvestLevel("axe", 0)
+        Blocks.SPONGE.setHarvestLevel("shovel", 0)
+        Blocks.STAINED_GLASS.setHarvestLevel("pickaxe", 0)
+        Blocks.STAINED_GLASS_PANE.setHarvestLevel("pickaxe", 0)
+        Blocks.STICKY_PISTON.setHarvestLevel("pickaxe", 0)
+    }
+
+    //Itemの最大スタック数を上書きする
+    private fun overrideStack() {
+        for (name in ForgeRegistries.ITEMS.keys) {
+            val item = RagiUtils.getItem(name.toString())
+            //itemの耐久値が0の場合、最大スタック数を64に上書きする
+            if (item.maxDamage == 0) item.setMaxStackSize(64)
+        }
     }
 }
