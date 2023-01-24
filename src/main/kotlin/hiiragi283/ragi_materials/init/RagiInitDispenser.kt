6442649888a913885e278ad8917447d3e0b60616
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-01-24 21:03:39 +0900

    Add internal class DispenseForgeFurnace

--- src/main/kotlin/hiiragi283/ragi_materials/init/RagiInitDispenser.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/init/RagiInitDispenser.kt
@@ -7,7 +7,7 @@ import net.minecraft.dispenser.IBlockSource
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 
-object RagiInitDispenser : BehaviorDefaultDispenseItem() {
+object RagiInitDispenser {
 
     /*
       Thanks to defeatedcrow!
@@ -17,36 +17,48 @@ object RagiInitDispenser : BehaviorDefaultDispenseItem() {
 
     //ディスペンサーに機能を登録するメソッド
     fun registerDispense() {
-        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.COAL, this) //燃料を投入
-        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FIRE_CHARGE, this) //着火
-        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FLINT_AND_STEEL, this) //着火
-        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(RagiInit.ItemToolBellow, this) //火力UP
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.COAL, DispenseForgeFurnace()) //燃料を投入
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FIRE_CHARGE, DispenseForgeFurnace()) //着火
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FLINT_AND_STEEL, DispenseForgeFurnace()) //着火
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(RagiInit.ItemToolBellow, DispenseForgeFurnace()) //火力UP
     }
 
-    override fun dispenseStack(source: IBlockSource, stack: ItemStack): ItemStack {
-        //各値の取得
-        val world = source.world
-        val pos = source.blockPos.offset(source.blockState.getValue(BlockDispenser.FACING))
-        val state = world.getBlockState(pos)
-        val block = state.block
-        //blockがForge Furnaceの場合
-        return if (block is BlockForgeFurnace) {
-            when (stack.item) {
-                Items.COAL -> {
-                    BlockForgeFurnace.ForgeFurnaceHelper.setFuel(world, pos, state, stack) //燃料を投入
-                    stack
+    //Forge Furnace用
+    class DispenseForgeFurnace : BehaviorDefaultDispenseItem() {
+        override fun dispenseStack(source: IBlockSource, stack: ItemStack): ItemStack {
+            //各値の取得
+            val world = source.world
+            val pos = source.blockPos.offset(source.blockState.getValue(BlockDispenser.FACING))
+            val state = world.getBlockState(pos)
+            val block = state.block
+            //blockがForge Furnaceの場合
+            return if (block is BlockForgeFurnace) {
+                when (stack.item) {
+                    Items.COAL -> {
+                        BlockForgeFurnace.ForgeFurnaceHelper.setFuel(world, pos, state, stack) //燃料を投入
+                        stack
+                    }
+
+                    Items.FIRE_CHARGE -> {
+                        BlockForgeFurnace.ForgeFurnaceHelper.setFireItem(world, pos, state, stack) //着火
+                        stack
+                    }
+
+                    Items.FLINT_AND_STEEL -> {
+                        BlockForgeFurnace.ForgeFurnaceHelper.setFireTool(world, pos, state, stack) //着火
+                        stack
+                    }
+
+                    RagiInit.ItemToolBellow -> {
+                        BlockForgeFurnace.ForgeFurnaceHelper.setBlasting(world, pos, state, stack) //火力UP
+                        stack
+                    }
+
+                    else -> super.dispenseStack(source, stack)
                 }
-                Items.FIRE_CHARGE -> {
-                    BlockForgeFurnace.ForgeFurnaceHelper.setFireItem(world, pos, state, stack) //着火
-                    stack
-                }
-                Items.FLINT_AND_STEEL -> {
-                    BlockForgeFurnace.ForgeFurnaceHelper.setFireTool(world, pos, state, stack) //着火
-                    stack
-                }
-                RagiInit.ItemToolBellow -> {
-                    BlockForgeFurnace.ForgeFurnaceHelper.setBlasting(world, pos, state, stack) //火力UP
-                    stack
+            } else super.dispenseStack(source, stack)
+        }
+    }
                 }
                 else -> super.dispenseStack(source, stack)
             }
