f4d7002d664e9f3b93fb17a2eed2d1ed767da558
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-01-24 21:04:18 +0900

    Fill or empty cauldron with dispenser

--- src/main/kotlin/hiiragi283/ragi_materials/init/RagiInitDispenser.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/init/RagiInitDispenser.kt
@@ -1,6 +1,8 @@
 package hiiragi283.ragi_materials.init
 
 import hiiragi283.ragi_materials.blocks.BlockForgeFurnace
+import hiiragi283.ragi_materials.util.RagiUtils
+import net.minecraft.block.BlockCauldron
 import net.minecraft.block.BlockDispenser
 import net.minecraft.dispenser.BehaviorDefaultDispenseItem
 import net.minecraft.dispenser.IBlockSource
@@ -17,6 +19,9 @@ object RagiInitDispenser {
 
     //ディスペンサーに機能を登録するメソッド
     fun registerDispense() {
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.WATER_BUCKET, DispenseCauldron()) //水バケツで水を搬入
+        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.BUCKET, DispenseCauldron()) //水バケツで水を搬出
+
         BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.COAL, DispenseForgeFurnace()) //燃料を投入
         BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FIRE_CHARGE, DispenseForgeFurnace()) //着火
         BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(Items.FLINT_AND_STEEL, DispenseForgeFurnace()) //着火
@@ -59,10 +64,35 @@ object RagiInitDispenser {
             } else super.dispenseStack(source, stack)
         }
     }
+
+    //大釜から水を出し入れする用
+    class DispenseCauldron : BehaviorDefaultDispenseItem() {
+        override fun dispenseStack(source: IBlockSource, stack: ItemStack): ItemStack {
+            //各値の取得
+            val world = source.world
+            val pos = source.blockPos.offset(source.blockState.getValue(BlockDispenser.FACING))
+            val state = world.getBlockState(pos)
+            val block = state.block
+            //blockが大釜の場合
+            return if (block is BlockCauldron) {
+                //stackが水入りバケツの場合
+                if (stack.item == Items.WATER_BUCKET) {
+                    //大釜が空の場合
+                    if (block.getMetaFromState(state) == 0) {
+                        world.setBlockState(pos, RagiUtils.getState(block, 3)) //大釜のblockstateを上書き
+                        ItemStack(Items.BUCKET) //空のバケツを返す
+                    } else super.dispenseStack(source, stack)
                 }
-                else -> super.dispenseStack(source, stack)
-            }
-        } else super.dispenseStack(source, stack)
+                //stackが水入りバケツの場合
+                else if (stack.item == Items.BUCKET) {
+                    //大釜が満タンの場合
+                    if (block.getMetaFromState(state) == 3) {
+                        world.setBlockState(pos, RagiUtils.getState(block, 0)) //大釜のblockstateを上書き
+                        ItemStack(Items.WATER_BUCKET) //水入りバケツを返す
+                    } else super.dispenseStack(source, stack)
+                } else super.dispenseStack(source, stack)
+            } else super.dispenseStack(source, stack)
+        }
     }
 
 }
