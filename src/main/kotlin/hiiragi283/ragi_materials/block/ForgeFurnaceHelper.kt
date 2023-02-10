5186c9ebc38886b451f45c0e2c180cec0b3f1f65
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-02-10 16:40:16 +0900

    Add alloy heating recipe

--- src/main/kotlin/hiiragi283/ragi_materials/block/ForgeFurnaceHelper.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/block/ForgeFurnaceHelper.kt
@@ -33,7 +33,10 @@ object ForgeFurnaceHelper {
         "minecraft:gold_ingot:0" to "ragi_materials:ingot_hot:79",
         "ragi_materials:ingot:79" to "ragi_materials:ingot_hot:79",
         "ragi_materials:ingot:82" to "ragi_materials:ingot_hot:82",
-        "ragi_materials:ingot:83" to "ragi_materials:ingot_hot:83"
+        "ragi_materials:ingot:83" to "ragi_materials:ingot_hot:83",
+        "ragi_materials:ingot:201" to "ragi_materials:ingot_hot:201",
+        "ragi_materials:ingot:211" to "ragi_materials:ingot_hot:211",
+        "ragi_materials:ingot:212" to "ragi_materials:ingot_hot:212"
     )
 
     var mapForgeBoosted = mutableMapOf(
@@ -45,19 +48,34 @@ object ForgeFurnaceHelper {
         "ragi_materials:ingot:27" to "ragi_materials:ingot_hot:27",
         "ragi_materials:ingot:28" to "ragi_materials:ingot_hot:28",
         "ragi_materials:ingot:40" to "ragi_materials:ingot_hot:40",
-        "ragi_materials:ingot:42" to "ragi_materials:ingot_hot:42",
         "ragi_materials:ingot:45" to "ragi_materials:ingot_hot:45",
         "ragi_materials:ingot:46" to "ragi_materials:ingot_hot:46",
-        "ragi_materials:ingot:78" to "ragi_materials:ingot_hot:78"
+        "ragi_materials:ingot:78" to "ragi_materials:ingot_hot:78",
+        "ragi_materials:ingot:200" to "ragi_materials:ingot_hot:200",
+        "ragi_materials:ingot:202" to "ragi_materials:ingot_hot:202",
+        "ragi_materials:ingot:203" to "ragi_materials:ingot_hot:203",
+        "ragi_materials:ingot:204" to "ragi_materials:ingot_hot:204",
+        "ragi_materials:ingot:205" to "ragi_materials:ingot_hot:205",
+        "ragi_materials:ingot:206" to "ragi_materials:ingot_hot:206",
+        "ragi_materials:ingot:207" to "ragi_materials:ingot_hot:207",
+        "ragi_materials:ingot:208" to "ragi_materials:ingot_hot:208",
+        "ragi_materials:ingot:209" to "ragi_materials:ingot_hot:209",
+        "ragi_materials:ingot:210" to "ragi_materials:ingot_hot:210",
+        "ragi_materials:ingot:213" to "ragi_materials:ingot_hot:213",
+        "ragi_materials:ingot:214" to "ragi_materials:ingot_hot:214",
+        "ragi_materials:ingot:216" to "ragi_materials:ingot_hot:216"
     )
 
     var mapForgeHellrise = mutableMapOf(
         "ragi_materials:ingot:41" to "ragi_materials:ingot_hot:41",
+        "ragi_materials:ingot:42" to "ragi_materials:ingot_hot:42",
         "ragi_materials:ingot:44" to "ragi_materials:ingot_hot:44",
         "ragi_materials:ingot:72" to "ragi_materials:ingot_hot:72",
         "ragi_materials:ingot:73" to "ragi_materials:ingot_hot:73",
         "ragi_materials:ingot:74" to "ragi_materials:ingot_hot:74",
-        "ragi_materials:ingot:76" to "ragi_materials:ingot_hot:76"
+        "ragi_materials:ingot:76" to "ragi_materials:ingot_hot:76",
+        "ragi_materials:ingot:77" to "ragi_materials:ingot_hot:77",
+        "ragi_materials:ingot:215" to "ragi_materials:ingot_hot:215"
     )
 
     //燃料を投入するメソッド
