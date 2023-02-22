fa007532dbc9f51f040b50fa733791d5d529b3f8
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-02-22 15:47:05 +0900

    MixtureBuilder generates special form of chemical formula

--- src/main/kotlin/hiiragi283/ragi_materials/material/MixtureBuilder.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/material/MixtureBuilder.kt
@@ -1,10 +1,22 @@
 package hiiragi283.ragi_materials.material
 
-class MixtureBuilder(index: Int, name: String, type: MaterialType, list: List<Any>): CompoundBuilder(index, name, type, list.associateBy(
-    { it } , { 1 })) {
+class MixtureBuilder(index: Int, name: String, type: MaterialType, private val list: List<Any>): MaterialBuilder(index, name, type) {
 
     init {
         color = null
+        formula = calcFormula()
         molar = null
+        register()
+    }
+
+    private fun calcFormula(): String {
+        //変数の宣言・初期化
+        var formula = ""
+        //list内の各要素に対して実行
+        for (i in list) {
+            if (i is MaterialBuilder) formula += ",${i.formula}"
+            else if (i is String) formula += ",$i"
+        }
+        return "(${formula.substring(1)})" //化学式を整形して返す
     }
 }
