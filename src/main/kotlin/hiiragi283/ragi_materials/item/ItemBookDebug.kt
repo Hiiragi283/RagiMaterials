2c378e720ba7bc20b670b6d6d8a4e87016f32d0e
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-02-24 21:44:25 +0900

    Refine Debug Book

--- src/main/kotlin/hiiragi283/ragi_materials/item/ItemBookDebug.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/item/ItemBookDebug.kt
@@ -9,8 +9,10 @@ import net.minecraft.item.EnumRarity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.ActionResult
 import net.minecraft.util.EnumActionResult
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.text.TextComponentString
 import net.minecraft.util.text.TextComponentTranslation
 import net.minecraft.world.World
 
@@ -22,7 +24,7 @@ class ItemBookDebug : ItemBase(Reference.MOD_ID, "book_debug", 2) {
         return EnumRarity.EPIC
     }
 
-    //アイテムを右クリックすると呼ばれるevent
+    //アイテムを右クリックすると呼ばれるメソッド
     override fun onItemRightClick(world: World, player: EntityPlayer, hand: EnumHand): ActionResult<ItemStack> {
         val stack: ItemStack = player.getHeldItem(hand)
         //デバッグモードの場合
@@ -61,5 +63,45 @@ class ItemBookDebug : ItemBase(Reference.MOD_ID, "book_debug", 2) {
             if (!world.isRemote) RagiUtils.executeCommand(player, "gamemode 1")
         } else if (!world.isRemote) player.sendMessage(TextComponentTranslation("text.ragi_materials.debug_disabled"))
         return ActionResult(EnumActionResult.SUCCESS, stack)
+    }
+
+    //アイテムをブロックに対して右クリックすると呼ばれるメソッド
+    override fun onItemUse(
+        player: EntityPlayer,
+        world: World,
+        pos: BlockPos,
+        hand: EnumHand,
+        facing: EnumFacing,
+        hitX: Float,
+        hitY: Float,
+        hitZ: Float
+    ): EnumActionResult {
+        //サーバー側&&デバッグ状態のみで実行
+        if (!world.isRemote && RagiConfig.debugMode.isDebug) {
+            //Property用
+            if (player.getHeldItem(hand).metadata == 2) {
+                //各値の取得
+                val state = world.getBlockState(pos)
+                val block = state.block
+                val meta = block.getMetaFromState(state)
+                player.sendMessage(TextComponentTranslation("text.ragi_materials.decoration_line"))
+                //ブロックの翻訳名をチャットに表示
+                player.sendMessage(TextComponentString("  §eName:§r§b " + ItemStack(block, 1, meta).displayName))
+                //ブロックのIDをチャットに表示
+                player.sendMessage(TextComponentString("  §eID:§r§b " + block.registryName))
+                //ブロックのBlockstateをチャットに表示
+                player.sendMessage(TextComponentString("  §eBlockstate:§r§b $state"))
+                //ブロックのHardnessをチャットに表示
+                player.sendMessage(TextComponentString("  §eHardness:§r§b " + state.getBlockHardness(world, pos)))
+                //ブロックのResistanceをチャットに表示
+                player.sendMessage(TextComponentString("  §eResistance:§r§b " + block.getExplosionResistance(player)))
+                //適正ツールをチャットに表示
+                player.sendMessage(TextComponentString("  §eHarvest Tool:§r§b " + block.getHarvestTool(state)))
+                //適正レベルをチャットに表示
+                player.sendMessage(TextComponentString("  §eHarvest Level:§r§b " + block.getHarvestLevel(state)))
+                player.sendMessage(TextComponentTranslation("text.ragi_materials.decoration_line"))
+            }
+        }
+        return EnumActionResult.SUCCESS
     }
 }
