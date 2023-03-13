4c622cc7b305ffa8e8859e6a6e4010bd043931b5
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-03-13 12:40:56 +0900

    Remove unused files

--- src/main/kotlin/hiiragi283/ragi_materials/unused/ToolProperty.kt
+++
@@ -1,9 +0,0 @@
-package hiiragi283.ragi_materials.unused
-
-import net.minecraft.item.Item
-
-class ToolProperty(val name: String, val level: Int, val durability: Int, val efficiency: Float, val damage: Float, val enchantability: Int) {
-
-    constructor(name: String, material: Item.ToolMaterial) : this(name, material.harvestLevel, material.maxUses, material.efficiency, material.attackDamage, material.enchantability)
-
-}
