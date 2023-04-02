6385adf9bde5bb3fb0fe86e05eec9d316b72e0ed
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-04-02 11:01:30 +0900

    Remove integration for CrT due to forge gradle update

--- src/main/kotlin/hiiragi283/ragi_materials/integration/IntegrationCore.kt
+++ src/main/kotlin/hiiragi283/ragi_materials/integration/IntegrationCore.kt
@@ -1,26 +1,21 @@
 package hiiragi283.ragi_materials.integration
 
 import hiiragi283.ragi_materials.config.RagiConfig
-import hiiragi283.ragi_materials.integration.crafttweaker.CraftTweakerCore
 import net.minecraftforge.fml.common.Loader
 
 object IntegrationCore {
 
-    val enableCT = Loader.isModLoaded("crafttweaker")
     val enableEIO = Loader.isModLoaded("enderio") && RagiConfig.integration.enableEIO
     val enableJEI = Loader.isModLoaded("jei")
     val enableMek = Loader.isModLoaded("mekanism") && RagiConfig.integration.enableMek
     val enableTE = Loader.isModLoaded("thermalexpansion") && RagiConfig.integration.enableTE
     val enableTF = Loader.isModLoaded("thermalfoundation") && RagiConfig.integration.enableTF
 
-    fun loadPreInit() {
-        if (enableCT) CraftTweakerCore.register()
-    }
+    fun loadPreInit() {}
 
     fun loadInit() {}
 
     fun loadPostInit() {
         PluginVanilla.loadPostInit()
-        if (enableCT) CraftTweakerCore.apply()
     }
 }
