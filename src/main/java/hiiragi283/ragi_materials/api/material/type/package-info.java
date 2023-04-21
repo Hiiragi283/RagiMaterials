f0013c9c2dac83af30417bf3477983cafe6600f8
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-04-21 19:31:13 +0900

    Removed java scripts

--- src/main/java/hiiragi283/ragi_materials/api/material/type/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(owner = RagiMaterials.MOD_ID, provides = "RagiMaterialsAPI|material|type", apiVersion = RagiMaterials.VERSION)
-package hiiragi283.ragi_materials.api.material.type;
-
-import hiiragi283.ragi_materials.RagiMaterials;
-import net.minecraftforge.fml.common.API;
--- src/main/java/hiiragi283/ragi_materials/api/registry/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(owner = RagiMaterials.MOD_ID, provides = "RagiMaterialsAPI|registry", apiVersion = RagiMaterials.VERSION)
-package hiiragi283.ragi_materials.api.registry;
-
-import hiiragi283.ragi_materials.RagiMaterials;
-import net.minecraftforge.fml.common.API;
