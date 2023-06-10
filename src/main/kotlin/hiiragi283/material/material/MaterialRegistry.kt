d313b066c5dd44dde78294a0c3cd5d5570cf306a
ko_no <90126004+MrKono@users.noreply.github.com>
2023-06-10 21:19:41 +0900

    [add] 1st & 2nd Period Elements

--- src/main/kotlin/hiiragi283/material/material/MaterialRegistry.kt
+++ src/main/kotlin/hiiragi283/material/material/MaterialRegistry.kt
@@ -23,6 +23,8 @@ object MaterialRegistry {
 
     //    Materials    //
 
+    // ELEMENTS //
+    // 1st Period
     @JvmField
     val HYDROGEN = HiiragiMaterial.Builder("hydrogen", 1)
         .setColor(RagiColor.BLUE)
@@ -32,8 +34,85 @@ object MaterialRegistry {
         .setTempMelt(14)
         .build()
 
+    @JvmField
+    val HELIUM = HiiragiMaterial.Builder("helium", 2)
+        .setColor(RagiColor.YELLOW)
+        .setFormula("He")
+        .setMolar(4.0)
+        .build()
+
+    // 2nd Period
+    @JvmField
+    val LITHIUM = HiiragiMaterial.Builder("lithium", 3)
+        .setColor(RagiColor.GRAY)
+        .setFormula("Li")
+        .setMolar(7.0)
+        .build()
+
+    @JvmField
+    val BERYLLIUM = HiiragiMaterial.Builder("beryllium", 4)
+        .setColor(RagiColor.DARK_GREEN)
+        .setFormula("Be")
+        .setMolar(9.0)
+        .build()
+
+    @JvmField
+    val BORON = HiiragiMaterial.Builder("boron", 5)
+        .setColor(RagiColor.GRAY)
+        .setFormula("B")
+        .setMolar(10.8)
+        .build()
+
+    @JvmField
+    val CARBON = HiiragiMaterial.Builder("carbon", 6)
+        .setColor(RagiColor.BLACK)
+        .setFormula("C")
+        .setMolar(12.0)
+        .build()
+
+    @JvmField
+    val NITROGEN = HiiragiMaterial.Builder("nitrogen", 7)
+        .setColor(RagiColor.DARK_AQUA)
+        .setFormula("N")
+        .setMolar(14.0)
+        .build()
+
+    @JvmField
+    val OXYGEN = HiiragiMaterial.Builder("oxygen", 8)
+        .setColor(RagiColor.AQUA)
+        .setFormula("O")
+        .setMolar(16.0)
+        .build()
+
+    @JvmField
+    val FLUORINE = HiiragiMaterial.Builder("fluorine", 9)
+        .setColor(RagiColor.BLUE)
+        .setFormula("F")
+        .setMolar(19.0)
+        .build()
+
+    @JvmField
+    val NEON = HiiragiMaterial.Builder("neon", 10)
+        .setColor(RagiColor.GREEN)
+        .setFormula("Ne")
+        .setMolar(20.2)
+        .build()
+
+
     fun init() {
+        // ELEMENTS //
+        // 1st Period
         registerMaterial(HYDROGEN)
+        registerMaterial(HELIUM)
+        // 2nd Period
+        registerMaterial(LITHIUM)
+        registerMaterial(BERYLLIUM)
+        registerMaterial(BORON)
+        registerMaterial(CARBON)
+        registerMaterial(NITROGEN)
+        registerMaterial(OXYGEN)
+        registerMaterial(FLUORINE)
+        registerMaterial(NEON)
     }
 
 }
