78226283761131f5f6895227a1b899693186ab47
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2023-07-16 21:48:51 +0900

    [WIP] Refining Config

--- src/main/kotlin/hiiragi283/material/RMEventHandler.kt
+++ src/main/kotlin/hiiragi283/material/RMEventHandler.kt
@@ -4,13 +4,27 @@ import hiiragi283.material.api.part.PartRegistry
 import net.minecraft.item.Item
 import net.minecraftforge.client.event.ColorHandlerEvent
 import net.minecraftforge.client.event.ModelRegistryEvent
+import net.minecraftforge.common.config.Config
+import net.minecraftforge.common.config.ConfigManager
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.event.entity.player.ItemTooltipEvent
+import net.minecraftforge.fml.client.event.ConfigChangedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
 
 object RMEventHandler {
+
+    /**
+     * @see <a href = https://github.com/ACGaming/UniversalTweaks/blob/main/src/main/java/mod/acgaming/universaltweaks/config/UTConfig.java>Reference </a>
+     */
+
+    @SubscribeEvent
+    fun onConfigChanged(event: ConfigChangedEvent.OnConfigChangedEvent) {
+        if (event.modID != RagiMaterials.MODID) return
+        ConfigManager.sync(RagiMaterials.MODID, Config.Type.INSTANCE)
+        RagiMaterials.LOGGER.info("Config Reloaded!")
+    }
 
     @SubscribeEvent
     fun registerItems(event: RegistryEvent.Register<Item>): Unit = RMItems.register(event.registry)
--- src/main/kotlin/hiiragi283/material/config/RMConfig.kt
+++ src/main/kotlin/hiiragi283/material/config/RMConfig.kt
@@ -6,6 +6,32 @@ import net.minecraftforge.common.config.Config
 @Config(modid = RagiMaterials.MODID)
 object RMConfig {
 
+    @Config.Name("Misc")
+    @JvmField
+    val MISC = Misc()
+
+    class Misc {
+
+        @Config.Name("Enable Lanthanides")
+        @Config.RequiresMcRestart
+        @JvmField
+        var LANTHANIDES: Boolean = true
+
+        @Config.Name("Enable Transuranium Elements")
+        @Config.RequiresMcRestart
+        @JvmField
+        var TRANSURAN: Boolean = true
+
+        @Config.Name("Generate Sample Json")
+        @JvmField
+        var SAMPLE_JSON: Boolean = true
+
+        @Config.Name("Print Registered Materials on Log")
+        @JvmField
+        var PRINT_MATERIALS: Boolean = false
+
+    }
+
     @JvmField
     var enableLanthanides: Boolean = true
 
